c247709cbb019ab888490437f95ab45d26433710
sidg767 <dsidhrt7@gmail.com>
2026-03-10 23:58:08 +0530

    Command parsing

---
+++ src/cmd.rs
@@ -0,0 +1,24 @@
+use anyhow::{Context,anyhow};
+use strum::{Display, EnumIs, EnumTryAs};
+#[derive(Debug, PartialEq, EnumIs, EnumTryAs, Display)]
+
+pub enum Cmd{
+  Echo(Comand),
+  Exit(u8),
+  Type(Command),
+  Exec(Command),
+  Unknown(Command),
+}
+
+impl Cmd{
+ pub fn try_as_command(& self) -> anyhow::Result<Command> {
+ match self{
+ Cmd::Echo(cmd)=> Ok(cmd::clone()),
+ Cmd::Type(cmd)=> Ok(cmd::clone()),
+ Cmd::Exec(cmd)=> Ok(cmd::clone()),
+ Cmd::Unknown(cmd)=> Ok(cmd::clone()),
+ Cmd::Exit(_)=>Err(anyhow!("exit cmd")),
+}
+}
+}
+
