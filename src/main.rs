e186cb1dad35124c3b8f2c584ca1099bc30ebd2c
sidg767 <dsidhrt7@gmail.com>
2026-03-12 23:28:40 +0530

    parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use pathsearch::find_executable_in_path;
 #[allow(unused_imports)]
+use pathsearch::find_executable_in_path;
 use std::io::{self, Write};
+use std::process::Command;
+use std::str;
 
 fn main() {
     loop {
@@ -8,8 +10,7 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut s = String::new();
         io::stdin().read_line(&mut s).unwrap();
-        s.pop();
-        let v: Vec<&str> = s.split_whitespace().collect();
+        let v: Vec<&str> = s.trim().split_whitespace().collect();
         if v.is_empty() {
             continue;
         }
@@ -22,10 +23,6 @@ fn main() {
 }
 fn eval_command(command: &str, args: Vec<&str>) {
     let comm_known = ["exit", "echo", "type"];
-    if !comm_known.contains(&command) {
-        println!("{}: command not found", &command);
-        return;
-    }
     if command == "echo" {
         for arg in &args {
             print!("{} ", arg);
@@ -33,13 +30,31 @@ fn eval_command(command: &str, args: Vec<&str>) {
         println!();
         return;
     }
+
     if command == "type" {
         if comm_known.contains(&args[0]) {
             println!("{} is a shell builtin", &args[0]);
+            return;
         } else if let Some(path) = find_executable_in_path(args[0]) {
             println!("{} is {}", args[0], path.display());
+            return;
         } else {
             println!("{}: not found", args[0]);
+            return;
         }
+    }
+    if let Some(_path) = find_executable_in_path(command) {
+        let output = Command::new(command)
+            .args(&args)
+            .output()
+            .expect("Invalid command");
+        if output.status.success() {
+            let stdout = str::from_utf8(&output.stdout).expect("Invalid UTF-8 output");
+            print!("{}", stdout);
+        } else {
+            println!("{}: command not found", &command);
+        }
+    } else {
+        println!("{}: command not found", &command);
     }
 }
