a2db217f0f5be9f43c499665d67060a3e873dfc8
sidg767 <dsidhrt7@gmail.com>
2026-05-01 16:46:10 +0530

    Added history to repl

--- src/cli/repl.rs
+++ src/cli/repl.rs
@@ -1,22 +1,37 @@
-use std::io::{self, Write};
+use rustyline::{Editor, Result};
+use crate::cli::completer::ShellCompleter;
+use crate::cli::history;
 use crate::lexer::tokenizer::form_tokens;
 use crate::exec::executor::eval_command;
-pub fn start() {
+
+pub fn start() -> Result<()> {
+    history::init_history().ok();
+    
+    let history_path = history::get_history_path_string();
+    let mut rl = Editor::new()?;
+    rl.set_helper(Some(ShellCompleter::new()));
+    
+    let _ = rl.load_history(&history_path);
+    
     loop {
-        print!("$ ");
-        io::stdout().flush().unwrap();
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        let mut tokens= {
-            form_tokens(input.trim())
-        };
-        if tokens.is_empty() {
-            continue;
+        let readline = rl.readline("$ ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(&line);
+                let mut tokens: Vec<String> = form_tokens(&line.trim().to_string());
+                if tokens.is_empty() {
+                    continue;
+                }
+                let command = tokens.remove(0);
+                if command == "exit" {
+                    break;
+                }
+                eval_command(&command, tokens);
+            }
+            Err(_) => break,
         }
-        let start = tokens.remove(0);   
-        if start == "exit" {
-            break;
-        }
-        eval_command(&start, tokens);
     }
-}   
+    
+    let _ = rl.save_history(&history_path);
+    Ok(())
+}
