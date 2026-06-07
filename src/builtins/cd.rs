c1d25f651678e7b590b1c2331a10191d0683ebdd
sidg767 <dsidhrt7@gmail.com>
2026-06-07 19:36:28 +0530

    removing panics

--- src/builtins/cd.rs
+++ src/builtins/cd.rs
@@ -1,13 +1,17 @@
 use std::env;
+use crate::error::shell_error::ShellError;
 
-pub fn run(args: &[String]) {
+pub fn run(args: &[String]) -> Result<i32, ShellError> {
     let home = if args.is_empty() || args[0] == "~" {
         env::var("HOME").unwrap_or("/".to_string())
     } else {
         args[0].clone()
     };
 
-    if let Err(_) = env::set_current_dir(&home) {
-        println!("cd: {}: No such file or directory", home);
+    if env::set_current_dir(&home).is_err() {
+        eprintln!("cd: {}: No such file or directory", home);
+        return Ok(1); // Usually cd failing sets exit code 1
     }
+    
+    Ok(0)
 }
